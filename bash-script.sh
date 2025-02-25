d0d9787415db0e20d57f40135f584c33359d1484
trem <128891674+EthicalTrem@users.noreply.github.com>
2025-02-25 22:26:16 +0200

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -1,5 +1,5 @@
-echo Scanning ports TCP 1-1100 on 3.160.246.90,3.160.246.37,3.160.246.8
-    nmap -oN scan_result.txt -p 1-1100 3.160.246.90 3.160.246.37 3.160.246.8 > nul
+echo Scanning ports TCP 1-1100 on Target-IP
+    nmap -oN scan_result.txt -p 1-1100 Target-IP > nul
     echo
     echo Scan complete. Result
     echo IP Address Port State Service
